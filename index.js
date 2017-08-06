70355c3ab41f936f2f50fc2aebc6eec0bf37cec4
vimrus <vimrus.com@gmail.com>
2017-08-06 19:46:52 +0800

    * fix.

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ $.get('http://www.yinwang.org', function(data, status, xhr) {
 				if (article) return;
 
 				$.get(url, function(data, status, xhr) {
+					var content = $(data).find('h2').nextAll();
 					var article = {
 						title: title,
-						content: $(data).find('td').eq(0).find('div').eq(1).html(),
-						summary: $(data).find('h2').next('p').val().substring(0, 30),
-						url: url,
-						time: ""
+						content: content.html(),
+						summary: content.text().substring(0, 50),
+						url: url
 					};
 					articles.append(article);
 				});
