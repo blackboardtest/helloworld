a46ee0135238d5610fe8130827efcc5e99fbc0de
vimrus <vimrus.com@gmail.com>
2017-08-14 00:14:02 +0800

    * fix.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-$.get('http://www.yinwang.org', function(data, status, xhr) {
-		$(data).find('.list-group a').each(function (index, value) {
+hubble.getHtml('http://www.yinwang.org', function(error, response, $) {
+		$('.list-group a').each(function (index, value) {
 			var url  = 'http://www.yinwang.org' + $(this).attr('href');
 			var title = $(this).text();
 			articles.get('url', url, function(article) {
 				if (article) return;
 
-				$.get(url, function(data, status, xhr) {
-					var html = $(data).find('td').eq(0).find('div').eq(1);
+				hubble.get(url, function(error, response, $) {
+					var html = $('td').eq(0).find('div').eq(1);
 					html.find('h2').remove()
 
-					var article = {
-						title: title,
-						content: html.html().trim(),
-						summary: html.text().trim().substring(0, 50),
-						url: url
-					};
+						var article = {
+							title: title,
+							content: html.html().trim(),
+							summary: html.text().trim().substring(0, 50),
+							url: url
+						};
 					articles.append(article);
 				});
 			});
